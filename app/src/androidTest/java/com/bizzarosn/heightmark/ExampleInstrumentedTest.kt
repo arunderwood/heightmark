48db545fd76b34267eceb2bedf055dc4e9d7ac90
arunderwood <arunderwood@users.noreply.github.com>
2025-07-06 17:41:53 -0700

    fix fragile test

--- app/src/androidTest/java/com/bizzarosn/heightmark/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bizzarosn/heightmark/ExampleInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.bizzarosn.heightmark
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,6 +17,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bizzarosn.heightmark", appContext.packageName)
+        assertTrue("Package name should start with com.bizzarosn.heightmark", 
+                   appContext.packageName.startsWith("com.bizzarosn.heightmark"))
     }
 }
